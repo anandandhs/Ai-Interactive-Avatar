aa2c40659ec78e6cfedc61a2e947de7e624c4788
ANAND\Anand <anandandhs@gmail.com>
2025-10-28 15:52:38 +0530

    Revert "marrie anne change"
    This reverts commit 3e156b9b5e2100d7cd1b6c30ef41725f55e64563.

--- components/InteractiveAvatar.tsx
+++ components/InteractiveAvatar.tsx
@@ -1041,8 +1041,7 @@ function InteractiveAvatar({ page }: { page: number }) {
             "john.keating@papyrrus.com" &&
             currentAvatarId === AVATARS[3].avatar_id &&
             selectedLanguage === "es" && {
-              // voiceId: "28f7220adbc144eeba42d70e1e969b29",
-              voiceId: "5f4fd40dfbe341bc9570f1ab10c647c0",
+              voiceId: "28f7220adbc144eeba42d70e1e969b29",
             }),
         },
         language:
